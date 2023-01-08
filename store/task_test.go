2e01259226f7c386bafed8922977902c080324ec
ryo0210 <yone0217@icloud.com>
2023-01-08 22:37:30 +0900

    add import

--- store/task_test.go
+++ store/task_test.go
@@ -97,6 +97,7 @@ func TestRepository_AddTask(t *testing.T) {
 	}
 	t.Cleanup(func() { _ = db.Close() })
 	mock.ExpectExec(
+		// エスケープが必要
 		`INSERT INTO task \(title, status, created, modified\) VALUES \(\?, \?, \?, \?\)`,
 	).WithArgs(okTask.Title, okTask.Status, c.Now(), c.Now()).WillReturnResult(sqlmock.NewResult(wantID, 1))
 
